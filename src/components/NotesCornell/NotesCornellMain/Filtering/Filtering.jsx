2df569f24b85f21c0877d458601ff20f557381dd
Virginia Velásquez Soto <virginiavelasquezsoto@gmail.com>
2019-04-06 17:20:32 -0500

    NotesCornell Progress Filtering  hoy-favor

--- src/components/NotesCornell/NotesCornellMain/Filtering/Filtering.jsx
+++ src/components/NotesCornell/NotesCornellMain/Filtering/Filtering.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators, compose } from 'redux';
 import { firestoreConnect } from 'react-redux-firebase';
@@ -13,36 +13,73 @@ import {
 } from '../../../../redux/actions/NotesCornellAction';
 
 const Filter = ({ notescornell, FilterMateria, FilterAll, FilterDate }) => {
+  const [isActiveCategAll, setActiveCategAll] = useState(1);
+  const [isActiveCategToday, setActiveCategToday] = useState(null);
+  const [isActiveCategFavor, setActiveCategFavor] = useState(null);
+  const [isActiveTag, setActiveTag] = useState(null);
+
   const dateNow = moment(Date.now())
     .locale('es')
     .format('LL');
 
-  const handleFilteAll = () => {
+  const handleFilteAll = ev => {
     FilterAll();
+    setActiveCategAll(1);
+    setActiveCategToday(null);
+    setActiveCategFavor(null);
+    setActiveTag(null);
   };
-  const handleFilteToday = () => {
+  const handleFilteToday = ev => {
     FilterDate(dateNow);
+    setActiveCategAll(null);
+    setActiveCategToday(2);
+    setActiveCategFavor(null);
+    setActiveTag(null);
   };
-  const handleFilteFavourite = () => {
-    console.log('Favorito');
+  const handleFilteFavourite = ev => {
+    setActiveCategAll(null);
+    setActiveCategToday(null);
+    setActiveCategFavor(3);
+    setActiveTag(null);
   };
-  const handleTagFilter = item => {
+  const handleTagFilter = (item, index) => {
     FilterMateria(item);
+    setActiveCategAll(null);
+    setActiveCategFavor(null);
+    setActiveTag(null);
+    setActiveTag(index);
   };
   const materia = notescornell && notescornell.map(item => item.materia);
   const tags = [...new Set(materia)].sort();
+
+  const classActiveAll = isActiveCategAll === 1 ? classes.Active : null;
+  const classActiveToday = isActiveCategToday === 2 ? classes.Active : null;
+  const classActiveFavor = isActiveCategFavor === 3 ? classes.Active : null;
+
   return (
     <div className={classes.Filtering}>
       <div className={classes.Menu}>
-        <button type="button" onClick={handleFilteAll} name="materia">
+        <button
+          type="button"
+          onClick={handleFilteAll}
+          name="1"
+          className={classActiveAll}>
           <i className="bx bx-grid-alt" />
           Todas
         </button>
-        <button type="button" onClick={handleFilteToday} name="date">
+        <button
+          type="button"
+          onClick={handleFilteToday}
+          name="2"
+          className={classActiveToday}>
           <i className="bx bx-calendar-event" />
           Hoy
         </button>
-        <button type="button" onClick={handleFilteFavourite} name="like">
+        <button
+          type="button"
+          onClick={handleFilteFavourite}
+          name="3"
+          className={classActiveFavor}>
           <i className="bx bx-star" />
           Favoritos
         </button>
@@ -50,16 +87,20 @@ const Filter = ({ notescornell, FilterMateria, FilterAll, FilterDate }) => {
       <div className={classes.Materias}>
         <h6>Etiquetas</h6>
         <ul>
-          {tags.map(item => (
-            <li key={item}>
-              <button
-                type="button"
-                name="materia"
-                onClick={() => handleTagFilter(item)}>
-                {item}
-              </button>
-            </li>
-          ))}
+          {tags.map((item, index) => {
+            const classActive = isActiveTag === index ? classes.Active : null;
+            return (
+              <li key={item}>
+                <button
+                  type="button"
+                  name="materia"
+                  className={classActive}
+                  onClick={() => handleTagFilter(item, index)}>
+                  {item}
+                </button>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
