40652acc3b32091abac21309631c7fd8e326cf77
Virginia Velásquez Soto <virginiavelasquezsoto@gmail.com>
2019-04-14 09:36:54 -0500

    Progress NCApunte  Editor part 11

--- src/components/UI/RichTextEditor/RichTextEditor.jsx
+++ src/components/UI/RichTextEditor/RichTextEditor.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Editor from 'draft-js-plugins-editor';
+import Editor, { composeDecorators } from 'draft-js-plugins-editor';
 import {
   ItalicButton,
   BoldButton,
@@ -12,17 +12,50 @@ import {
 } from 'draft-js-buttons';
 import createToolbarPlugin, { Separator } from 'draft-js-static-toolbar-plugin';
 import createImagePlugin from 'draft-js-image-plugin';
-
+import createAlignmentPlugin from 'draft-js-alignment-plugin';
+import createFocusPlugin from 'draft-js-focus-plugin';
+import createResizeablePlugin from 'draft-js-resizeable-plugin';
+import createBlockDndPlugin from 'draft-js-drag-n-drop-plugin';
+import createDragNDropUploadPlugin from '@mikeljames/draft-js-drag-n-drop-upload-plugin';
 import HeadlinesButton from './Toolbar/HeadlinesButton';
 import ImageAdd from './Toolbar/AddImageEditor';
-
+import createColorBlockPlugin from './Toolbar/colorBlockPlugin';
+import 'draft-js-alignment-plugin/lib/plugin.css';
 import 'draft-js-static-toolbar-plugin/lib/plugin.css';
 import classes from './RichTextEditor.module.scss';
 
-const imagePlugin = createImagePlugin();
+const focusPlugin = createFocusPlugin();
+const resizeablePlugin = createResizeablePlugin();
+const blockDndPlugin = createBlockDndPlugin();
+const alignmentPlugin = createAlignmentPlugin();
+const { AlignmentTool } = alignmentPlugin;
 const staticToolbarPlugin = createToolbarPlugin();
 const { Toolbar } = staticToolbarPlugin;
-const plugins = [staticToolbarPlugin, imagePlugin];
+
+const decorator = composeDecorators(
+  resizeablePlugin.decorator,
+  alignmentPlugin.decorator,
+  focusPlugin.decorator,
+  blockDndPlugin.decorator
+);
+
+const imagePlugin = createImagePlugin({ decorator });
+const colorBlockPlugin = createColorBlockPlugin({ decorator });
+
+const dragNDropFileUploadPlugin = createDragNDropUploadPlugin({
+  addImage: imagePlugin.addImage,
+});
+
+const plugins = [
+  staticToolbarPlugin,
+  imagePlugin,
+  dragNDropFileUploadPlugin,
+  blockDndPlugin,
+  focusPlugin,
+  alignmentPlugin,
+  resizeablePlugin,
+  colorBlockPlugin,
+];
 
 class RichTextEditor extends Component {
   render() {
@@ -48,6 +81,7 @@ class RichTextEditor extends Component {
                 onChange={onChange}
                 modifier={imagePlugin.addImage}
               />
+              <AlignmentTool />
             </React.Fragment>
           )}
         </Toolbar>
