d6c14d1bce15d45f6858f44cdd5a9f8e0e7637c1
Virginia Velásquez Soto <virginiavelasquezsoto@gmail.com>
2019-05-10 09:59:37 -0500

    Proceso Document part 8

--- src/components/Documentos/Documento/DocumentoVideo/DocumentoVideo.jsx
+++ src/components/Documentos/Documento/DocumentoVideo/DocumentoVideo.jsx
@@ -7,23 +7,31 @@ import classes from './DocumentoVideo.module.scss';
 
 import { getTimelineVideoDoc } from '../../../../redux/actions/DocumentosAction';
 
-const DocumentVideo = ({ timeline }) => (
-  <div className={classes.DocumentVideo}>
-    <div className={classes.boxVideo}>
-      <VideoPlayer srcVideo="https://firebasestorage.googleapis.com/v0/b/dpipette-ff5ee.appspot.com/o/notescornell%2Feconomia%2Fciencia_economica%2Fresumen?alt=media&token=14e72533-38ae-4715-94fd-327846c32410" />
-    </div>
-    <div className={classes.boxAddTimeVideo}>
-      <button type="button" className="btn btn-primary">
-        <i className="bx bx-plus" />
-      </button>
-      <div className={classes.timeVideo}>
-        <p>
-          <strong>{timeline}</strong>
-        </p>
+const DocumentVideo = ({ timeline, getTimelineVideoDoc }) => {
+  const handleTimeline = () => {
+    getTimelineVideoDoc(3);
+  };
+  return (
+    <div className={classes.DocumentVideo}>
+      <div className={classes.boxVideo}>
+        <VideoPlayer srcVideo="https://firebasestorage.googleapis.com/v0/b/dpipette-ff5ee.appspot.com/o/notescornell%2Feconomia%2Fciencia_economica%2Fresumen?alt=media&token=14e72533-38ae-4715-94fd-327846c32410" />
+      </div>
+      <div className={classes.boxAddTimeVideo}>
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={handleTimeline}>
+          <i className="bx bx-plus" />
+        </button>
+        <div className={classes.timeVideo}>
+          <p>
+            <strong>{timeline}</strong>
+          </p>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 const mapDispatchToProps = dispatch =>
   bindActionCreators({ getTimelineVideoDoc }, dispatch);
