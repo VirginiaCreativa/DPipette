c855890d588cabc69268eb4f97adcb29d93f1a7e
Virginia Velásquez Soto <virginiavelasquezsoto@gmail.com>
2019-07-29 19:26:03 -0500

    SG popup imagenes part I

--- src/components/Significados/SignificadosDetail/ImagenesDetail/ImagenesDetail.jsx
+++ src/components/Significados/SignificadosDetail/ImagenesDetail/ImagenesDetail.jsx
@@ -1,14 +1,46 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import SkyLight from 'react-skylight';
+
 import classes from './ImagenesDetail.module.scss';
 
-const ImagenesDetail = ({ imagenes, word }) => (
-  <div className={classes.ImagenesDetail}>
-    {imagenes.map(item => (
-      <div className={classes.ImagenOverw} key={item}>
-        <img src={item} alt={word} />
-      </div>
-    ))}
-  </div>
-);
+const ImagenesDetail = ({ imagenes, word }) => {
+  const animated = useRef(null);
+
+  const handlePopImg = () => {
+    animated.current.show();
+    console.log('dmjflaksdj');
+  };
+
+  const classPopup = {
+    width: '65%',
+    minHeight: '600px',
+    top: '30%',
+    left: '20%',
+    marginTop: '-100px',
+    marginLeft: '-30px',
+  };
+  const classClosedNone = { display: 'none' };
+  return (
+    <div className={classes.ImagenesDetail}>
+      {imagenes.map(item => (
+        <div
+          className={classes.ImagenOverw}
+          key={item}
+          role="button"
+          tabIndex="0"
+          onClick={handlePopImg}>
+          <img src={item} alt={word} />
+        </div>
+      ))}
+      <SkyLight
+        hideOnOverlayClicked
+        dialogStyles={classPopup}
+        closeButtonStyle={classClosedNone}
+        ref={animated}>
+        Hello, I dont have any callback.
+      </SkyLight>
+    </div>
+  );
+};
 
 export default ImagenesDetail;
